d67deadd64fe7d7a143dc174e9ce8fdba251f39a
Alex Luo <aluo50@stuy.edu>
2024-09-25 10:30:16 -0400

    added updated code

--- 09_softserve/app.py
+++ 09_softserve/app.py
@@ -1,10 +1,10 @@
-'''
-Alex Luo
-ADD
-SoftDev
-K09 -- Display occupations at random on the loopback interface from the occupations csv file
-2024-09-23
-time spent: 1.2
+heading = '''
+Alex Luo <br>
+ADD <br>
+SoftDev <br>
+K09 -- Display occupations at random on the loopback interface from the occupations csv file <br>
+2024-09-23 <br>
+Time spent: 0.3 <br>
 '''
 
 import random
@@ -16,18 +16,23 @@ app = Flask(__name__)
 
 def randfxn():
     f = open("occupations.csv", "r").read()
-    f = f.split("\n")[1:-1]
+    f = f.split("\n")[1:-1] # Turns data into list
     
-    print(len(f))
     flist = []
     
     for i in f:
-        i = i.split(",")[:-1]
-        i = ",".join(i).replace('"',"")
+        i = i.split(",")[:-1] # Turns list elements into sublists while getting rid of the percentages
+        i = ",".join(i).replace('"',"") # Sublists are joined back into strings while extra "" are removed
         flist.append(i)
     
     print(__name__)
-    num = random.randint(0, len(flist)-1)
-    return flist[num]
+    num = random.randint(0, len(flist)-2) # Needs to exclude the "Total" name
+
+    fstring = "Occupations List: <br><br>"
+
+    for i in flist[:-1]: # Turns the list into a string
+        fstring += i + "<br>"
+
+    return heading + "<br> " + fstring + "<br>Random Occupation:<br> " + flist[num]
 
 app.run()
