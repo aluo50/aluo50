da4000a9ec503a09b1d7100bea6b44a902cf01f3
Alex Luo <aluo50@stuy.edu>
2024-09-27 10:40:27 -0400

    added 11_flask-static

--- 11_flask-static/app.py
+++ 11_flask-static/app.py
@@ -1,3 +1,5 @@
+# Alex Luo
+# TummyAKE
 # Clyde 'Thluffy' Sinclair
 # SoftDev
 # Sep 2024
@@ -7,22 +9,24 @@
 import random
 from flask import Flask
 app = Flask(__name__)
+
+with open('static/fixie.html', 'r') as f:
+    html_string = f.read()
+
 '''
 @app.route("/")
 def hello_world():
     print("the __name__ of this module is... ")
     print(__name__)
     return "No hablo queso!"
+
 '''
 
-
-@app.route("/static/foo.html") # route to serve static files from the /static folder 
-
+@app.route("/static/fixie.html")
 def h():
     print("the __name__ of this module is... ")
     print(__name__)
-    return str(random.random())
-
+    return str(html_string)
 
 
 if __name__ == "__main__":  # true if this file NOT imported
