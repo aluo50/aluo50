523917a7fcdebfbb530a4ba74d0d4a9491d19c92
Alex Luo <aluo50@stuy.edu>
2024-09-30 10:31:50 -0400

    added comment

--- 12_flask-jinja/app.py
+++ 12_flask-jinja/app.py
@@ -29,7 +29,7 @@ foo, collection --> data we pass to the html web page through this python script
 # Q0: What will happen if you remove render_template from the following statement?
 # (log prediction before executing...)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
@@ -50,5 +50,5 @@ def test_tmplt():
 
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = True # when website faces public, debug should be false so your code is not exposed
     app.run()
